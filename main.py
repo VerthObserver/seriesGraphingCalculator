ad2ce56a33e6afbc7a87c2a4c1cc3c668c110d25
Laevateinn <verthandi.observer@gmail.com>
2022-10-29 18:44:42 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+
+from math import factorial as fact
+import numpy as np
+import matplotlib.pyplot as plt
+
+limit = 10
+
+n = np.arange(0, limit, 1)
+seq = 1/2**n
+ser = np.zeros(shape=limit)
+ser[0] = seq[0]
+
+for i in range(0, limit - 1):
+    ser[i + 1] = (ser[i] + seq[i + 1])
+
+print(f"Indices: {n}")
+print(f"\nSequence: {seq}")
+print(f"\nSeries: {ser}")
+
+
+fig, (ax1, ax2) = plt.subplots(2)
+
+plt.suptitle("Series and sequences")
+
+ax1.bar(n, seq, color='r')
+ax1.set_title("Sequence")
+ax1.title.set_color('r')
+
+ax2.bar(n, ser, color='b')
+ax2.set_title("Series")
+ax2.title.set_color('b')
+
+plt.show()
